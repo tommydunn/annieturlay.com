4eb9659e3992ebf40cd6ee9b840a9eef58eafc2e
tommyldunn <tommylogandunn@gmail.com>
2018-04-04 07:49:42 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AboutMeComponent } from './about-me/about-me.component';
-import { PlayerComponent } from './player.component';
 import { HeroOneComponent } from './hero-one/hero-one.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutMeComponent,
-    PlayerComponent,
     HeroOneComponent
   ],
   imports: [
