9ba22edfb6e80964a4b57eb968502dddb7c375fd
tommyldunn <tommylogandunn@gmail.com>
2018-04-04 22:09:32 -0400

    more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 import { Project } from './project';
 import { PROJECTS } from './projects';
 
@@ -10,29 +12,45 @@ import { PROJECTS } from './projects';
 
 export class AppComponent implements OnInit {
   mouseEvent;
-  id = 'KJamzD0KntE';
-  private player;
-  private ytEvent;
   projects = PROJECTS;
-  selectedProject: Project;
+  project: Project;
+
+  public currentIndex: number;
+  public selectedProject: Project = this.projects[this.currentIndex];
+
+  private id = '';
+  private player: any;
+  private ytEvent: any;
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e) {
-    this.mouseEvent = e;
+    // this.mouseEvent = e;
   }
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.selectedProject = null;
   }
 
-  onSelect(project: Project): void {
+  onSelect(project: Project, index: number): void {
+    this.currentIndex = index;
     this.selectedProject = project;
   }
 
   onStateChange(event) {
     this.ytEvent = event.data;
   }
+
+  stopVideo(): void {
+    this.player.stopVideo();
+  }
+
+  exitPlayer(): void {
+    // this.closeModal('custom-modal-1');
+    this.selectedProject = null;
+  }
+
   savePlayer(player) {
     this.player = player;
   }
@@ -43,5 +61,6 @@ export class AppComponent implements OnInit {
 
   pauseVideo() {
     this.player.pauseVideo();
+    this.selectedProject = null;
   }
 }
