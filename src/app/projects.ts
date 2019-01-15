79538b0d222202a6ce1e6de0fb410a457c13bc02
tommydunn <tommylogandunn@gmail.com>
2019-01-14 22:42:49 -0800

    NEW SPOTS NEW ORDER

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -2,6 +2,13 @@ import { Project } from './project';
 
 export const PROJECTS: Project[] = [
   {
+    name: 'Infiniti',
+    title: 'Rules of Luxury',
+    director: 'Adam Hashemi - Reset',
+    youtubeid: '-SOfV1DiQI8',
+    image: 'assets/images/splash/rules-of-luxury.png',
+    tag: ''
+  }, {
     name: 'Letgo',
     title: 'Cliff',
     director: 'Noam Murro - Biscuit',
@@ -9,46 +16,18 @@ export const PROJECTS: Project[] = [
     image: 'assets/images/splash/letgo-cliff-splash.jpg',
     tag: ''
   }, {
-    name: 'Kraft Cracker Barrel',
-    title: 'PrestigiousAwards',
-    director: 'Milana Vayntrub - Hungry Man',
-    youtubeid: 'hTWdYiCduWo',
-    image: 'assets/images/splash/kraft-cracker-barrel-prestigious-awards-splash.jpg',
+    name: 'Kraft Macaroni & Cheese',
+    title: 'swear like a mother',
+    director: 'Hank Perlman - Hungry Man',
+    youtubeid: 'GUbwahg4yac',
+    image: 'assets/images/splash/kraft-swear-splash.jpg',
     tag: ''
   }, {
-    name: 'American Airlines',
-    title: 'Employee Video',
-    director: 'Chris McPherson - Hungry Man',
-    youtubeid: 'P8QkqhBbfRw',
-    image: 'assets/images/splash/american-airlines-employee-video.jpg',
-    tag: ''
-  }, {
-    name: 'Applebees',
-    title: 'Showboatin Chef',
-    director: 'Irv Blitz - MJZ ',
-    youtubeid: 'djDxI0Q-Fpc',
-    image: 'assets/images/splash/applebees-showboatin-chef-splash.jpg',
-    tag: ''
-  }, {
-    name: 'BarkThins',
-    title: 'Direct hit',
-    director: 'Noble/Screen Novelties',
-    youtubeid: 'k5sj8kCm2ag',
-    image: 'assets/images/splash/barkthins-direct-hit.jpg',
-    tag: ''
-  }, {
-    name: 'BarkThins',
-    title: 'Yoga',
-    director: 'Noble - Screen Novelties',
-    youtubeid: 'KZcN_DYSAKs',
-    image: 'assets/images/splash/barkthins-yoga.jpg',
-    tag: ''
-  }, {
-    name: 'Charles Schwab',
-    title: 'The Reason',
-    director: 'John Hillcoat - Skunk',
-    youtubeid: 'vqq7y0rjTIk',
-    image: 'assets/images/splash/charles-schwab-the-reason.jpg',
+    name: 'Ice Breakers',
+    title: 'Dance Circle',
+    director: 'Steve Ayson - MJZ ',
+    youtubeid: '1TodDRuOaso',
+    image: 'assets/images/splash/dance-circle.png',
     tag: ''
   }, {
     name: 'Fruit of the Loom',
@@ -56,139 +35,6 @@ export const PROJECTS: Project[] = [
     director: 'Martin Granger - Moxie',
     youtubeid: 'cdtWfcxgm-Q',
     image: 'assets/images/splash/fruit-of-the-loom-plasticpants.jpg',
-    tag: ''
-  }, {
-    name: 'Ice Breakers',
-    title: 'Cement Shoes',
-    director: 'Steve Ayson - MJZ ',
-    youtubeid: '3ZGKkapTadc',
-    image: 'assets/images/splash/ice-breakers-cement-shoes.jpg',
-    tag: ''
-  }, {
-    name: 'Kraft Macaroni & Cheese',
-    title: 'Bounce House',
-    director: 'Harry Cocciolo - Bob Industries',
-    youtubeid: 'GB0bLbPmzxA',
-    image: 'assets/images/splash/kraft-bounce-house.jpg',
-    tag: ''
-  }, {
-    name: 'applebees',
-    title: 'retired rivals',
-    director: 'Irv Blitz - MJZ ',
-    youtubeid: 'l_-s67qLz3E',
-    image: 'assets/images/splash/applebees-retired-rivals.jpg',
-    tag: ''
-  }, {
-    name: 'velveeta',
-    title: 'declaration',
-    director: 'The Nix Brothers',
-    youtubeid: 'wpXyI3WPBn4',
-    image: 'assets/images/splash/velveeta-declaration.jpg',
-    tag: ''
-  }, {
-    name: 'applebees',
-    title: 'showboatin chef 60',
-    director: 'Irv Blitz - MJZ ',
-    youtubeid: 'Q3s9GeSO24w',
-    image: 'assets/images/splash/applebees-showboatin-chef-60.jpg',
-    tag: ''
-  }, {
-    name: 'belvita',
-    title: 'breakfast police',
-    director: 'Miguel Arteta - Bob Industries',
-    youtubeid: 'NQNvfIROBmg',
-    image: 'assets/images/splash/belvita-breakfast-police.jpg',
-    tag: ''
-  }, {
-    name: 'belvita',
-    title: 'yellow jackets',
-    director: 'Miguel Arteta - Bob Industries',
-    youtubeid: '3Ua1AqvXmOQ',
-    image: 'assets/images/splash/belvita-yellow-jackets.jpg',
-    tag: ''
-  }, {
-    name: 'fruit of the loom',
-    title: 'plastique teaser frank',
-    director: '',
-    youtubeid: '8O-DKjuMOoE',
-    image: 'assets/images/splash/fruit-of-the-loom-plastique-teaser-frank.jpg',
-    tag: ''
-  }, {
-    name: 'fruit of the loom',
-    title: 'plastique',
-    director: '',
-    youtubeid: 'T26S4Gaan90',
-    image: 'assets/images/splash/fruit-of-the-loom-plastique.jpg',
-    tag: ''
-  }, {
-    name: 'hotels.com',
-    title: 'vacation equality project',
-    director: 'Brent Bonacorso - Tool',
-    youtubeid: 'TdLEsPjs6tY',
-    image: 'assets/images/splash/hotels-com-vacation-equality-project.jpg',
-    tag: ''
-  }, {
-    name: 'jello',
-    title: 'puddin hour',
-    director: 'Pelorian Brothers - MJZ',
-    youtubeid: 'mBvFhBRIsEQ',
-    image: 'assets/images/splash/jello-puddin-hour.jpg',
-    tag: ''
-  }, {
-    name: 'kraft macaroni & cheese',
-    title: 'diamond jubilee',
-    director: 'Hank Perlman - Hungry Man',
-    youtubeid: 'Txz3tJ8bx9Q',
-    image: 'assets/images/splash/kmc-diamond-jubilee.jpg',
-    tag: ''
-  }, {
-    name: 'old navy',
-    title: 'flare to spare',
-    director: 'Roman Coppola',
-    youtubeid: 'TKg24Vw4NDU',
-    image: 'assets/images/splash/old-navy-flare-to-spare.jpg',
-    tag: ''
-  }, {
-    name: 'rock shox',
-    title: 'bike',
-    director: 'Jason Macdonald - no6',
-    youtubeid: 'QhRN7BtCtJw',
-    image: 'assets/images/splash/rock-shox-bike.jpg',
-    tag: ''
-  }, {
-    name: 'applebees',
-    title: 'outtakes from productions',
-    director: 'Misc.',
-    youtubeid: 'Y8t60BFFPvQ',
-    image: 'assets/images/splash/applebees-outtakes-from-productions.jpg',
-    tag: ''
-  }, {
-    name: 'kraft macaroni & cheese',
-    title: 'spectator shapes disney cars',
-    director: 'Harry Cocciolo - Bob Industries',
-    youtubeid: '77ge7LnkDNM',
-    image: 'assets/images/splash/kmc-spectator-shapes-disney-cars.jpg',
-    tag: ''
-  }, {
-    name: 'Infiniti',
-    title: 'Alexa',
-    director: 'Christian Lamb - Scheme Engine',
-    youtubeid: 'Smq6ssI-nH8',
-    image: 'assets/images/splash/infiniti-splash.jpg',
-    tag: ''
-  }, {
-    name: 'Hotels.com',
-    title: 'April Fools',
-    director: 'Wade Brothers',
-    youtubeid: 'qsVUr-OOA0M',
-    image: 'assets/images/splash/hotels-april-fools-splash.jpg',
-    tag: ''
-  }, {
-    name: 'Ice Breakers',
-    title: 'Text',
-    director: 'Steve Ayson - MJZ ',
-    youtubeid: 'jAB-DR0EZDk',
-    image: 'assets/images/splash/ice-breakers-text-splash.jpg',
     tag: ''
   }, {
     name: 'JELLO',
@@ -205,18 +51,60 @@ export const PROJECTS: Project[] = [
     image: 'assets/images/splash/kraft-cant-play-splash.jpg',
     tag: ''
   }, {
-    name: 'Kraft Macaroni & Cheese',
-    title: 'swear like a mother',
-    director: 'Hank Perlman - Hungry Man',
-    youtubeid: 'GUbwahg4yac',
-    image: 'assets/images/splash/kraft-swear-splash.jpg',
+    name: 'Charles Schwab',
+    title: 'The Reason',
+    director: 'John Hillcoat - Skunk',
+    youtubeid: 'vqq7y0rjTIk',
+    image: 'assets/images/splash/charles-schwab-the-reason.jpg',
     tag: ''
   }, {
-    name: 'Fruit of the Loom',
-    title: 'PlasticPockets',
-    director: 'Martin Granger - Moxie',
-    youtubeid: 'QzuyB5hA-PY',
-    image: 'assets/images/splash/fruitloom-plastic-pockets-splash.jpg',
+    name: 'belvita',
+    title: 'breakfast police',
+    director: 'Miguel Arteta - Bob Industries',
+    youtubeid: 'NQNvfIROBmg',
+    image: 'assets/images/splash/belvita-breakfast-police.jpg',
+    tag: ''
+  }, {
+    name: 'Applebees',
+    title: 'Showboatin Chef',
+    director: 'Irv Blitz - MJZ ',
+    youtubeid: 'djDxI0Q-Fpc',
+    image: 'assets/images/splash/applebees-showboatin-chef-splash.jpg',
+    tag: ''
+  }, {
+    name: 'Kraft Cracker Barrel',
+    title: 'PrestigiousAwards',
+    director: 'Milana Vayntrub - Hungry Man',
+    youtubeid: 'hTWdYiCduWo',
+    image: 'assets/images/splash/kraft-cracker-barrel-prestigious-awards-splash.jpg',
+    tag: ''
+  }, {
+    name: 'Ice Breakers',
+    title: 'Cement Shoes',
+    director: 'Steve Ayson - MJZ ',
+    youtubeid: '3ZGKkapTadc',
+    image: 'assets/images/splash/ice-breakers-cement-shoes.jpg',
+    tag: ''
+  }, {
+    name: 'kraft macaroni & cheese',
+    title: 'diamond jubilee',
+    director: 'Hank Perlman - Hungry Man',
+    youtubeid: 'Txz3tJ8bx9Q',
+    image: 'assets/images/splash/kmc-diamond-jubilee.jpg',
+    tag: ''
+  }, {
+    name: 'BarkThins',
+    title: 'Direct hit',
+    director: 'Noble/Screen Novelties',
+    youtubeid: 'k5sj8kCm2ag',
+    image: 'assets/images/splash/barkthins-direct-hit.jpg',
+    tag: ''
+  }, {
+    name: 'velveeta',
+    title: 'declaration',
+    director: 'The Nix Brothers',
+    youtubeid: 'wpXyI3WPBn4',
+    image: 'assets/images/splash/velveeta-declaration.jpg',
     tag: ''
   }, {
     name: 'Letgo',
@@ -226,11 +114,25 @@ export const PROJECTS: Project[] = [
     image: 'assets/images/splash/letgo-fire-splash.jpg',
     tag: ''
   }, {
-    name: 'Letgo',
-    title: 'Camping',
-    director: 'Noam Murro - Biscuit',
-    youtubeid: 'SXYIdxgvr7Q',
-    image: 'assets/images/splash/letgo-camping-splash.jpg',
+    name: 'old navy',
+    title: 'flare to spare',
+    director: 'Roman Coppola',
+    youtubeid: 'TKg24Vw4NDU',
+    image: 'assets/images/splash/old-navy-flare-to-spare.jpg',
+    tag: ''
+  }, {
+    name: 'Hotels.com',
+    title: 'April Fools',
+    director: 'Wade Brothers',
+    youtubeid: 'qsVUr-OOA0M',
+    image: 'assets/images/splash/hotels-april-fools-splash.jpg',
+    tag: ''
+  }, {
+    name: 'American Airlines',
+    title: 'Employee Video',
+    director: 'Chris McPherson - Hungry Man',
+    youtubeid: 'P8QkqhBbfRw',
+    image: 'assets/images/splash/american-airlines-employee-video.jpg',
     tag: ''
   }, {
     name: 'Applebees',
@@ -238,6 +140,48 @@ export const PROJECTS: Project[] = [
     director: 'Jared Hess - Community Films ',
     youtubeid: 'xvWRwdYHxmY',
     image: 'assets/images/splash/applebees-flash-mob-splash.jpg',
+    tag: ''
+  }, {
+    name: 'Infiniti',
+    title: 'Alexa',
+    director: 'Christian Lamb - Scheme Engine',
+    youtubeid: 'Smq6ssI-nH8',
+    image: 'assets/images/splash/infiniti-splash.jpg',
+    tag: ''
+  }, {
+    name: 'fruit of the loom',
+    title: 'plastique',
+    director: '',
+    youtubeid: 'T26S4Gaan90',
+    image: 'assets/images/splash/fruit-of-the-loom-plastique.jpg',
+    tag: ''
+  }, {
+    name: 'rock shox',
+    title: 'bike',
+    director: 'Jason Macdonald - no6',
+    youtubeid: 'QhRN7BtCtJw',
+    image: 'assets/images/splash/rock-shox-bike.jpg',
+    tag: ''
+  }, {
+    name: 'hotels.com',
+    title: 'vacation equality project',
+    director: 'Brent Bonacorso - Tool',
+    youtubeid: 'TdLEsPjs6tY',
+    image: 'assets/images/splash/hotels-com-vacation-equality-project.jpg',
+    tag: ''
+  }, {
+    name: 'applebees',
+    title: 'showboatin chef 60',
+    director: 'Irv Blitz - MJZ ',
+    youtubeid: 'Q3s9GeSO24w',
+    image: 'assets/images/splash/applebees-showboatin-chef-60.jpg',
+    tag: ''
+  }, {
+    name: 'jello',
+    title: 'puddin hour',
+    director: 'Pelorian Brothers - MJZ',
+    youtubeid: 'mBvFhBRIsEQ',
+    image: 'assets/images/splash/jello-puddin-hour.jpg',
     tag: ''
   }
 ];
